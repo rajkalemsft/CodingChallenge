1860c19b82f0d9a2d9d4c92237b04b6ef963dbdd
Raj Kale <rajenka@microsoft.com>
2022-02-14 16:16:11 -0800

    tree

---
+++ DataStructures/Tree/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Tree
+{
+    internal class Node <T>
+    {
+        public T Data { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+    }
+}

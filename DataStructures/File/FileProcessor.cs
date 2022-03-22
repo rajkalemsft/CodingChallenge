f3802dd487265d679d02f0e6f4141a0e44ef4aea
Raj Kale <rajenka@microsoft.com>
2022-03-21 23:12:28 -0700

    Added commit

---
+++ DataStructures/File/FileProcessor.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DataStructures
+{
+    public class FileProcessor
+    {
+        public FileProcessor()
+        {
+
+        }
+        
+        public void CreateFile()
+        {
+            using (FileStream fs = File.Create(@"C:\OriginalFile.txt"))
+            {
+                for (int i = 0; i < 100000000; i++)
+                {
+                    AddText(fs, i + " This is some text");
+                    AddText(fs, " \r\n" + i + " and this is on a new line");
+                }
+            }
+        }
+
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
+        }
+
+
+        public void SplitFile(string inputFile = "C:\\OriginalFile.txt")
+        {
+            List<string> Packets = new List<string>();
+            int nNoofFiles = 0;
+
+            try
+            {
+                FileStream fs = new FileStream(inputFile, FileMode.Open, FileAccess.Read);
+                double fileSize = (double)fs.Length / 1024;
+                nNoofFiles = (int)Math.Ceiling(fileSize / (1000 * 1000));
+                int SizeofEachFile = (int)Math.Ceiling((double)fs.Length / nNoofFiles);
+
+                for (int i = 0; i < nNoofFiles; i++)
+                {
+                    string baseFileName = Path.GetFileNameWithoutExtension(inputFile);
+                    string Extension = Path.GetExtension(inputFile);
+
+                    FileStream outputFile = new FileStream(Path.GetDirectoryName(inputFile) + "\\" + baseFileName + "." +
+                        i.ToString().PadLeft(5, Convert.ToChar("0")) + Extension + ".tmp", FileMode.Create, FileAccess.Write);
+
+                    var mergeFolder = Path.GetDirectoryName(inputFile);
+
+                    int bytesRead = 0;
+                    byte[] buffer = new byte[SizeofEachFile];
+
+                    if ((bytesRead = fs.Read(buffer, 0, SizeofEachFile)) > 0)
+                    {
+                        outputFile.Write(buffer, 0, bytesRead);
+                        //outp.Write(buffer, 0, BytesRead);
+
+                        string packet = baseFileName + "." + i.ToString().PadLeft(3, Convert.ToChar("0")) + Extension.ToString();
+                        Packets.Add(packet);
+                    }
+
+                    outputFile.Close();
+
+                }
+                fs.Close();
+            }
+            catch (Exception Ex)
+            {
+                throw new ArgumentException(Ex.Message);
+            }
+        }
+
+        public bool MergeFile(string inputfoldername1)
+        {
+            bool Output = false;
+
+            try
+            {
+                string[] tmpfiles = Directory.GetFiles(inputfoldername1, "*.tmp");
+
+                FileStream outPutFile = null;
+                string PrevFileName = "";
+
+                foreach (string tempFile in tmpfiles)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(tempFile);
+                    string baseFileName = fileName.Substring(0, fileName.IndexOf(Convert.ToChar(".")));
+                    string extension = Path.GetExtension(fileName);
+
+                    if (!PrevFileName.Equals(baseFileName))
+                    {
+                        if (outPutFile != null)
+                        {
+                            outPutFile.Flush();
+                            outPutFile.Close();
+                        }
+                        outPutFile = new FileStream("D:\\" + "\\" + baseFileName + extension, FileMode.OpenOrCreate, FileAccess.Write);
+
+                    }
+
+                    int bytesRead = 0;
+                    byte[] buffer = new byte[1024];
+                    FileStream inputTempFile = new FileStream(tempFile, FileMode.OpenOrCreate, FileAccess.Read);
+
+                    while ((bytesRead = inputTempFile.Read(buffer, 0, 1024)) > 0)
+                        outPutFile.Write(buffer, 0, bytesRead);
+
+                    inputTempFile.Close();
+                    File.Delete(tempFile);
+                    PrevFileName = baseFileName;
+
+                }
+
+                outPutFile.Close();
+            }
+            catch
+            {
+
+            }
+
+            return Output;
+
+        }
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,15 +1,13 @@
-﻿using DataStructures.LinkedList;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace DataStructures
+﻿namespace DataStructures
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var file = new FileProcessor();
+            file.CreateFile();
+            file.SplitFile();
+
             //string[] planets1 = { "Mercury", "Venus", "Earth", "Jupiter" };
             //string[] planets2 = { "Mercury", "Earth", "Mars", "Jupiter" };
 
@@ -20,191 +18,191 @@ namespace DataStructures
             //    Console.WriteLine(str);
             //}
 
-            #region RorateArray by 'N' elements
+            //#region RorateArray by 'N' elements
 
-            var arr = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //var arr = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-            ArrayOps.RotateArray(arr, 4);
+            //ArrayOps.RotateArray(arr, 4);
 
-            #endregion
+            //#endregion
 
-            #region Rearrange array to have elements with matching index, in case element is not found in array have it as -1
-            var arrToarrange = new int[10] { -1, -1, 6, 1, 9, 3, 2, -1, 4, -1 };
+            //#region Rearrange array to have elements with matching index, in case element is not found in array have it as -1
+            //var arrToarrange = new int[10] { -1, -1, 6, 1, 9, 3, 2, -1, 4, -1 };
 
-            ArrayOps.RearrangeArray(arrToarrange);
-            #endregion
+            //ArrayOps.RearrangeArray(arrToarrange);
+            //#endregion
 
-            #region Reverse Array
-            var arrToReverse = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //#region Reverse Array
+            //var arrToReverse = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-            ArrayOps.ReverseArray(arrToReverse);
+            //ArrayOps.ReverseArray(arrToReverse);
 
-            //var arr2 = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            ////var arr2 = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-            //ArrayOps.ReverseArray(arr2);
-            #endregion
+            ////ArrayOps.ReverseArray(arr2);
+            //#endregion
 
-            #region Generic Queue
-            var queue = new LinkListQueue<double>();
-            queue.Queue(1.1);
-            queue.Queue(2.1);
-            queue.Queue(3.1);
-            queue.Queue(4.1);
+            //#region Generic Queue
+            //var queue = new LinkListQueue<double>();
+            //queue.Queue(1.1);
+            //queue.Queue(2.1);
+            //queue.Queue(3.1);
+            //queue.Queue(4.1);
 
-            queue.DeQueue();
-            #endregion
+            //queue.DeQueue();
+            //#endregion
 
-            #region Generic Stack
-            var stack = new LinkListStack<double>();
+            //#region Generic Stack
+            //var stack = new LinkListStack<double>();
 
-            stack.Push(1.1);
-            stack.Push(2.1);
-            stack.Push(3.1);
-            stack.Push(4.1);
-            stack.Pop();
-            #endregion
+            //stack.Push(1.1);
+            //stack.Push(2.1);
+            //stack.Push(3.1);
+            //stack.Push(4.1);
+            //stack.Pop();
+            //#endregion
 
-            #region LinkedList
-            SinglyLinkedList llist = new SinglyLinkedList();
+            //#region LinkedList
+            //SinglyLinkedList llist = new SinglyLinkedList();
 
-            llist.InsertNode(1);
-            llist.InsertNode(2);
-            llist.InsertNode(3);
-            llist.InsertNode(4);
-            llist.InsertNode(5);
+            //llist.InsertNode(1);
+            //llist.InsertNode(2);
+            //llist.InsertNode(3);
+            //llist.InsertNode(4);
+            //llist.InsertNode(5);
 
-            llist.Reverse();
-            var list = new LinkList<int>();
+            //llist.Reverse();
+            //var list = new LinkList<int>();
 
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            list.Add(4);
-            list.InsertAt(10, 1);
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(3);
+            //list.Add(4);
+            //list.InsertAt(10, 1);
 
-            var circularList = new CircularLinkList<int>();
+            //var circularList = new CircularLinkList<int>();
 
-            circularList.Add(1);
-            circularList.Add(2);
-            circularList.Add(3);
-            circularList.Add(4);
-            circularList.IsCircular(list.Head);
+            //circularList.Add(1);
+            //circularList.Add(2);
+            //circularList.Add(3);
+            //circularList.Add(4);
+            //circularList.IsCircular(list.Head);
 
-            circularList.ToCirular();
+            //circularList.ToCirular();
 
-            circularList.IsCircular(list.Head);
+            //circularList.IsCircular(list.Head);
 
-            var listTocheck = new LinkList<int>();
+            //var listTocheck = new LinkList<int>();
 
-            listTocheck.Add(5);
-            listTocheck.Add(6);
-            listTocheck.Add(7);
-            listTocheck.Add(1);
+            //listTocheck.Add(5);
+            //listTocheck.Add(6);
+            //listTocheck.Add(7);
+            //listTocheck.Add(1);
 
-            var issorted = listTocheck.IsSortedList;
-            #endregion
+            //var issorted = listTocheck.IsSortedList;
+            //#endregion
 
-            #region String Reversal
-            var outStr = StringOps.ReverseString("RajendraKalew");
+            //#region String Reversal
+            //var outStr = StringOps.ReverseString("RajendraKalew");
 
-            #endregion
+            //#endregion
 
-            #region Binry Search
-            var input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            var returnValue1 = BinarySearch<int>.Search(input, 1);
-            var returnValue2 = BinarySearch<int>.Search(input, 2);
-            var returnValue3 = BinarySearch<int>.Search(input, 3);
-            var returnValue4 = BinarySearch<int>.Search(input, 4);
-            var returnValue5 = BinarySearch<int>.Search(input, 5);
-            var returnValue6 = BinarySearch<int>.Search(input, 6);
-            var returnValue7 = BinarySearch<int>.Search(input, 7);
-            var returnValue8 = BinarySearch<int>.Search(input, 8);
-            var returnValue9 = BinarySearch<int>.Search(input, 9);
-            var returnValue10 = BinarySearch<int>.Search(input, 10);
+            //#region Binry Search
+            //var input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //var returnValue1 = BinarySearch<int>.Search(input, 1);
+            //var returnValue2 = BinarySearch<int>.Search(input, 2);
+            //var returnValue3 = BinarySearch<int>.Search(input, 3);
+            //var returnValue4 = BinarySearch<int>.Search(input, 4);
+            //var returnValue5 = BinarySearch<int>.Search(input, 5);
+            //var returnValue6 = BinarySearch<int>.Search(input, 6);
+            //var returnValue7 = BinarySearch<int>.Search(input, 7);
+            //var returnValue8 = BinarySearch<int>.Search(input, 8);
+            //var returnValue9 = BinarySearch<int>.Search(input, 9);
+            //var returnValue10 = BinarySearch<int>.Search(input, 10);
 
-            var inputStrArr = new string[] { "abc", "def", "hij", "klm", "nop" };
-            var returnValue11 = BinarySearch<string>.Search(inputStrArr, "abc");
-            var returnValue21 = BinarySearch<string>.Search(inputStrArr, "def");
-            var returnValue31 = BinarySearch<string>.Search(inputStrArr, "hij");
-            var returnValue41 = BinarySearch<string>.Search(inputStrArr, "klm");
-            var returnValue51 = BinarySearch<string>.Search(inputStrArr, "nop");
-            var returnValue61 = BinarySearch<string>.Search(inputStrArr, "qrs");
-            #endregion
+            //var inputStrArr = new string[] { "abc", "def", "hij", "klm", "nop" };
+            //var returnValue11 = BinarySearch<string>.Search(inputStrArr, "abc");
+            //var returnValue21 = BinarySearch<string>.Search(inputStrArr, "def");
+            //var returnValue31 = BinarySearch<string>.Search(inputStrArr, "hij");
+            //var returnValue41 = BinarySearch<string>.Search(inputStrArr, "klm");
+            //var returnValue51 = BinarySearch<string>.Search(inputStrArr, "nop");
+            //var returnValue61 = BinarySearch<string>.Search(inputStrArr, "qrs");
+            //#endregion
 
-            #region Recursion
-            var sumList = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //#region Recursion
+            //var sumList = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            var sum = Recursion.SumListItems(sumList);
+            //var sum = Recursion.SumListItems(sumList);
 
-            var countList = new int[] { 1 };
+            //var countList = new int[] { 1 };
 
-            var count = Recursion.CountNumberOfItemsInList(countList);
+            //var count = Recursion.CountNumberOfItemsInList(countList);
 
-            var maxList = new int[] { -1, -2, -3, -4 };
+            //var maxList = new int[] { -1, -2, -3, -4 };
 
-            var maxNum = Recursion.MaximumNumberInList(maxList);
+            //var maxNum = Recursion.MaximumNumberInList(maxList);
 
-            #endregion
+            //#endregion
 
-            #region Sorting
-            var inputArr = new int[] { 5, 4, 1, 3, 18, 21, -1 };
+            //#region Sorting
+            //var inputArr = new int[] { 5, 4, 1, 3, 18, 21, -1 };
 
-            QuickSort.Sort(inputArr, 0, inputArr.Length - 1);
+            //QuickSort.Sort(inputArr, 0, inputArr.Length - 1);
 
-            var inputArr2 = new int[] { 56, 93, 63, 42, 54, 9, 1, 4, 6, 2 };
+            //var inputArr2 = new int[] { 56, 93, 63, 42, 54, 9, 1, 4, 6, 2 };
 
-            MergeSort.Sort(inputArr2);
+            //MergeSort.Sort(inputArr2);
 
-            #endregion
+            //#endregion
 
-            #region Tree Traversal
+            //#region Tree Traversal
 
-            var tree = new Tree(10);
-            tree.Add(8);
-            tree.Add(9);
-            tree.Add(4);
-            tree.Add(5);
-            tree.Add(3);
-            tree.Add(17);
-            tree.Add(14);
-            tree.Add(11);
-            tree.Add(12);
-            tree.Add(19);
-            tree.Add(18);
-            tree.Add(20);
+            //var tree = new Tree(10);
+            //tree.Add(8);
+            //tree.Add(9);
+            //tree.Add(4);
+            //tree.Add(5);
+            //tree.Add(3);
+            //tree.Add(17);
+            //tree.Add(14);
+            //tree.Add(11);
+            //tree.Add(12);
+            //tree.Add(19);
+            //tree.Add(18);
+            //tree.Add(20);
 
-            tree.InOrderTraversal(tree.Root);
-            tree.PreOrderTraversal(tree.Root);
-            tree.PostOrderTraversal(tree.Root);
+            //tree.InOrderTraversal(tree.Root);
+            //tree.PreOrderTraversal(tree.Root);
+            //tree.PostOrderTraversal(tree.Root);
 
-            #endregion
+            //#endregion
 
-            #region Tree Key Value Store
-            try
-            {
-                var treekv = new TreeKv();
-                treekv.Create("/first_level", "value1");
-                treekv.Create("/sibling", "value2");
-                treekv.Create("/first", "value1");
-                treekv.Create("/first/second", "value2");
-                treekv.Create("/first/third", "value3");
-                var value = treekv.Get("/first/third");
-                var value1 = treekv.Set("/first/third", "valueReplaced");
-                treekv.Delete("/first/second");
-                treekv.Delete("/fourth");
+            //#region Tree Key Value Store
+            //try
+            //{
+            //    var treekv = new TreeKv();
+            //    treekv.Create("/first_level", "value1");
+            //    treekv.Create("/sibling", "value2");
+            //    treekv.Create("/first", "value1");
+            //    treekv.Create("/first/second", "value2");
+            //    treekv.Create("/first/third", "value3");
+            //    var value = treekv.Get("/first/third");
+            //    var value1 = treekv.Set("/first/third", "valueReplaced");
+            //    treekv.Delete("/first/second");
+            //    treekv.Delete("/fourth");
 
-            }
-            catch (System.Exception ex)
-            {
+            //}
+            //catch (System.Exception ex)
+            //{
 
-            }
+            //}
 
-            #endregion
+            //#endregion
 
-            var temp = Math.Round((decimal)3 / 6, 6);
-            //Result.plusMinus(new List<int> { -4, 3, -9, 0, 4, 1});
+            //var temp = Math.Round((decimal)3 / 6, 6);
+            ////Result.plusMinus(new List<int> { -4, 3, -9, 0, 4, 1});
 
-          
+
         }
 
     }
